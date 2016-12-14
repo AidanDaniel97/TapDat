85daea00c6498fd1bb67251ab54775e7d7b366a6
Aidan Daniel <aidan.d@wearedrum.com>
2016-12-14 14:48:24 +0000

    no message

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
- 
+var express = require('express'); 
+var app = express(); 
 var port = 2000;
 var server = app.listen(port)
 var io = require('socket.io').listen(server);
 
- 
-
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+app.use(express.static('./'));
+app.get('/', function(req, res){
+	res.send('index.html'); 
+}); 
  
 var leaders = [];
 
